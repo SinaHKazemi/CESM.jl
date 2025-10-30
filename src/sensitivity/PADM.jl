1e9fd0b3ec5d0a6521c6307e857f0fef1923abee
Sina Hajikazemi <sina.hkazemi@gmail.com>
2025-10-30 17:47:11 +0100

    PADM completed

--- src/sensitivity/PADM.jl
+++ src/sensitivity/PADM.jl
@@ -8,12 +8,12 @@ using JuMP, Dualization, Gurobi
 
 DUALITY_GAP_THRESHOLD = 0.001
 CONVERGENCE_THRESHOLD = 0.001
-INITIAL_MU = .001
-INCREASE_RATE = 0.05
-MANIPULATION_LIMIT = 0.1
+INITIAL_MU = .1
+INCREASE_RATE = 0.1
+MANIPULATION_LIMIT = 0.10
 MU_INCREASE_FACTOR = 2
 MAX_INNER_ITER = 40
-MAX_OUTER_ITER = 100
+MAX_OUTER_ITER = 30
 
 CHANGED_PROFILE_PROCESS = "Demand_Electricity"
 CHANGED_CAPACITY_PROCESS = "PP_PV"
@@ -40,7 +40,7 @@ function add_upper_constrs(input,primal_model,upper_vars, changed_profile_proces
         @constraint(primal_model, upper_vars["abs"][t] >= -upper_vars["delta"][t], base_name="abs_lower_bound_$(t)")
         @constraint(primal_model, upper_vars["abs"][t] <= MANIPULATION_LIMIT, base_name="manipulation_limit_$(t)")
     end
-    @constraint(primal_model, sum(upper_vars["delta"][t] * get_parameter(input,"output_profile",(changed_profile_process,t)) for t in input.timesteps)==0, base_name="total_change")
+    # @constraint(primal_model, sum(upper_vars["delta"][t] * get_parameter(input,"output_profile",(changed_profile_process,t)) for t in input.timesteps)==0, base_name="total_change")
 end
 
 function add_manipulation_constrs(input,primal_model,primal_vars,changed_capacity_process, capacity)
@@ -54,6 +54,18 @@ function set_primal_constrs(input, primal_model, primal_vars, upper_vars, primal
             primal_constrs["load_shape"][changed_profile_process,y,t] = @constraint(
                 primal_model,
                 primal_vars["energy_out_time"][changed_profile_process,y,t] == get_parameter(input,"output_profile",(changed_profile_process,t)) * get_parameter(input,"min_energy_out",(changed_profile_process,y)) * (1+upper_vars["delta"][t]),
+                base_name = "load_shape_$(changed_profile_process)_$(y)_$(t)"
+            )
+        end
+    end
+end
+
+function set_primal_constrs(input, primal_model, primal_vars, primal_constrs, changed_profile_process)
+    for y in input.years
+        for t in input.timesteps
+            primal_constrs["load_shape"][changed_profile_process,y,t] = @constraint(
+                primal_model,
+                primal_vars["energy_out_time"][changed_profile_process,y,t] == get_parameter(input,"output_profile",(changed_profile_process,t)) * get_parameter(input,"min_energy_out",(changed_profile_process,y)),
                 base_name = "load_shape_$(changed_profile_process)_$(y)_$(t)"
             )
         end
@@ -77,8 +89,6 @@ function update_primal_objective(input, mu, dual_vars, primal_model, primal_obj,
 end
 
 function update_dual_obj(input, dual_model, upper_values, dual_vars, changed_profile_process)
-    println(objective_sense(dual_model))
-    println(objective_function(dual_model))
     for y in input.years
         for t in input.timesteps
             set_objective_coefficient(dual_model, dual_vars[y,t], get_parameter(input,"output_profile",(changed_profile_process,t)) * get_parameter(input,"min_energy_out",(changed_profile_process,y)) * (1+upper_values[t]))
@@ -86,61 +96,112 @@ function update_dual_obj(input, dual_model, upper_values, dual_vars, changed_pro
     end
 end
 
+function check_obj_coeff(input, dual_model, dual_vars, changed_profile_process)
+    f = objective_function(dual_model)
+    for y in input.years
+        for t in input.timesteps
+            if get_parameter(input,"output_profile",(changed_profile_process,t)) * get_parameter(input,"min_energy_out",(changed_profile_process,y)) == coefficient(f, dual_vars[y,t])
+                println(get_parameter(input,"output_profile",(changed_profile_process,t)) * get_parameter(input,"min_energy_out",(changed_profile_process,y)))
+                println(coefficient(f, dual_vars[y,t]))
+                println(y)
+                println(t)
+            end
+        end
+    end
+end
+
+
+function test_primal(input, input_without_profile, changed_profile_process, upper_values)
+    input = deepcopy(input)
+    for t in input.timesteps
+        input.parameters["output_profile"][changed_profile_process][t] *= (1+ upper_values[t])
+    end
+    (primal_model,primal_vars,primal_constrs) = CESM.Model.build_model(input_without_profile)
+    set_primal_constrs(input, primal_model, primal_vars, primal_constrs, changed_profile_process)
+    set_attribute(primal_model, "OutputFlag", 0)
+    optimize!(primal_model)
+    output = CESM.Model.get_output(input, primal_vars)
+    # capacity = sum(get(output["new_capacity"],(changed_capacity_process,y),0) for y in input.years if Int(y)<=2050)
+    # println(capacity)
+    println("primal test: $(objective_value(primal_model))")
+end
+
 
 function PADM_alg(input)
     changed_profile_process =  first(filter(p -> p.name == CHANGED_PROFILE_PROCESS, input.processes))
     changed_capacity_process = first(filter(p -> p.name == CHANGED_CAPACITY_PROCESS, input.processes))
 
-    (primal_model,primal_vars,primal_constrs) = CESM.Model.build_model(input)
-    # set_attribute(primal_model, "OutputFlag", 0)
+    input_without_profile = deepcopy(input)
+    delete!(input_without_profile.parameters["output_profile"],changed_profile_process)
+
+
+    (primal_model,primal_vars,primal_constrs) = CESM.Model.build_model(input_without_profile)
+    set_primal_constrs(input, primal_model, primal_vars, primal_constrs, changed_profile_process)
+    set_attribute(primal_model, "OutputFlag", 0)
     optimize!(primal_model)
     output = CESM.Model.get_output(input, primal_vars)
     
     capacity = sum(get(output["new_capacity"],(changed_capacity_process,y),0) for y in input.years if Int(y)<=2050)
+    println(capacity)
 
     
     dual_model = dualize(primal_model,Gurobi.Optimizer; dual_names = DualNames("dual_var_", "dual_con_"))
     dual_vars = get_dual_vars(input, dual_model, changed_profile_process)
     dual_obj = objective_function(dual_model)
     set_attribute(dual_model, "Method", 2)
-    # set_attribute(dual_model, "OutputFlag", 0)
+    set_attribute(dual_model, "OutputFlag", 0)
     optimize!(dual_model)
     
-    output_profile = input.parameters["output_profile"][changed_profile_process] 
-    delete!(input.parameters["output_profile"],changed_profile_process)
-    (primal_model,primal_vars,primal_constrs) = CESM.Model.build_model(input)
-    input.parameters["output_profile"][changed_profile_process] = output_profile
+
+    (primal_model,primal_vars,primal_constrs) = CESM.Model.build_model(input_without_profile)
     primal_obj = objective_function(primal_model)
-    # set_attribute(primal_model, "OutputFlag", 0)
+    set_attribute(primal_model, "OutputFlag", 0)
     upper_vars = add_upper_vars(input, primal_model)
     upper_obj = upper_vars["obj"]
     add_upper_constrs(input, primal_model, upper_vars, changed_profile_process)
     add_manipulation_constrs(input,primal_model,primal_vars,changed_capacity_process, capacity)
     set_primal_constrs(input, primal_model, primal_vars, upper_vars, primal_constrs, changed_profile_process)
     optimize!(primal_model)
-    println("solve finished !!!!!!####")
 
     mu = INITIAL_MU
     upper_values = Dict(t => 0 for t in input.timesteps)
     println("algorithm started")
+    outer_counter = 0
     while true
+        println("Outer Loop: $(outer_counter)")
+        outer_counter += 1
+        inner_counter = 0
         while true
+            println("Inner Loop: $(inner_counter)")
+            inner_counter += 1
             update_primal_objective(input, mu, dual_vars, primal_model, primal_obj, upper_vars, upper_obj, changed_profile_process)
             optimize!(primal_model)
-            update_dual_obj(input, dual_model, upper_values, dual_vars, changed_profile_process)
+            new_upper_values = Dict(t => value(upper_vars["delta"][t]) for t in input.timesteps)
+            update_dual_obj(input, dual_model, new_upper_values, dual_vars, changed_profile_process)
             optimize!(dual_model)
-            if maximum(values(Dict(t => abs(value(upper_vars["delta"][t]) - upper_values[t]) for t in input.timesteps))) < CONVERGENCE_THRESHOLD
+            
+
+            println("primal obj: $(value(primal_obj))")
+            # test_primal(input, input_without_profile, changed_profile_process, new_upper_values)
+            dual_obj = objective_function(dual_model)
+            println("dual obj: $(value(dual_obj))")
+            println("upper obj: $(value(upper_obj))")
+            println(maximum(values(Dict(t => abs(new_upper_values[t] - upper_values[t]) for t in input.timesteps))))
+            if maximum(values(Dict(t => abs(new_upper_values[t] - upper_values[t]) for t in input.timesteps))) < CONVERGENCE_THRESHOLD
                 break
             else
-                upper_values = Dict(t => value(upper_vars["delta"][t]) for t in input.timesteps)
+                upper_values = new_upper_values
             end
         end
-        if (value(primal_obj) - value(dual_obj)) < DUALITY_GAP_THRESHOLD
+        if abs(value(primal_obj) - value(dual_obj)) < DUALITY_GAP_THRESHOLD || outer_counter > MAX_OUTER_ITER
             break
         else
             mu *= MU_INCREASE_FACTOR
         end
     end
+    println("primal obj: $(value(primal_obj))")
+    println("dual obj: $(value(dual_obj))")
+    println("upper obj: $(value(upper_obj))")
     return upper_values
 end
 
